9f74c12cb2fd0cba8846665a6c296bac0e6e613f
Anna Matuszewska <a.matuszewska01@gmail.com>
2026-04-27 17:44:56 +0200

    Anti-bot version

--- test_ing.py
+++ test_ing.py
@@ -17,15 +17,20 @@ def test_ing_accept_analytical_cookies(page: Page):
         "Accept-Language": "pl-PL,pl;q=0.9,en-US;q=0.8,en;q=0.7"
     })
     page.goto("https://www.ing.pl/", wait_until="networkidle", timeout=60000)
+    
     try:
         dostosuj_btn = page.get_by_role("button", name=re.compile(r"Dostosuj", re.IGNORECASE))
-        expect(dostosuj_btn).to_be_visible(timeout=20000)
+        expect(dostosuj_btn).to_be_visible(timeout=15000)
         dostosuj_btn.click()
-        page.get_by_role("heading", name=re.compile(r"Cookies analityczne", re.IGNORECASE)).click()
-        page.get_by_role("button", name=re.compile(r"Zaakceptuj zaznaczone", re.IGNORECASE)).last.click()
-        
+        analityczne_header = page.get_by_role("heading", name=re.compile(r"Cookies analityczne", re.IGNORECASE))
+        analityczne_header.click()
+        accept_btn = page.get_by_role("button", name=re.compile(r"Zaakceptuj", re.IGNORECASE)).last
+        accept_btn.click()
         page.wait_for_timeout(2000)
         expect(dostosuj_btn).not_to_be_visible()
-        
+        cookies = page.context.cookies()
+        assert len(cookies) > 0, "Błąd: Przeglądarka nie zapisała żadnych ciasteczek."
+
     except Exception as e:
+        print("TimeoutError czyli Anti-Bot na serwerze CI.")
         raise e
